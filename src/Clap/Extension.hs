9884787b71538e94494be28965ed74f9ce07c7cb
Joe Betz <joebetz91@gmail.com>
2023-01-12 17:07:34 +0100

    start implementation of host's get_extension callback

--- src/Clap/Extension.hs
+++ src/Clap/Extension.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE MultiWayIf #-}
+
 module Clap.Extension where
 
-import Clap.Interface.Plugin
+import Clap.Interface.Plugin as Plugin
 import Clap.Interface.Extension.Foreign.Gui
 import Clap.Interface.Extension.Gui as Gui
 import Clap.Interface.Extension.Log as Log
@@ -20,4 +22,14 @@ initializeExtensions plugin = Extensions
     <*> initializeExtension PluginRenderHandle Render.extensionId
     where 
         initializeExtension wrapper id =
-            fmap (wrapper . castPtr) <$> getExtension plugin id 
+            fmap (wrapper . castPtr) <$> Plugin.getExtension plugin id 
+
+getExtension :: Extensions -> String -> Ptr ()
+getExtension extensions name = if
+    | name == Gui.extensionId -> extract extensions_gui unPluginGuiHandle
+    | name == Log.extensionId -> extract extensions_log unHostLogHandle
+    | name == Render.extensionId -> extract extensions_render unPluginRenderHandle
+    | otherwise -> nullPtr
+    where
+        extract selector unWrapper =
+            maybe nullPtr (castPtr . unWrapper) (selector extensions)
