25ef8060b62c52415e53403118b3f12e268a7832
Joe Betz <joebetz91@gmail.com>
2023-01-19 01:25:04 +0100

    create playNote example

---
+++ src/Clap/Example.hs
@@ -0,0 +1,29 @@
+module Clap.Example where
+
+import Clap.Engine as Engine
+import Clap.Interface.Events
+import Clap.Interface.Host (defaultHostConfig)
+import Control.Concurrent
+
+playNote :: IO ()
+playNote = do
+  engine <- createEngine defaultHostConfig
+  let pluginId = ("plugins\\clap-saw-demo.clap", 0)
+  putStrLn "loadPlugin"
+  loadPlugin engine pluginId
+  putStrLn "start"
+  startResult <- start engine
+  print startResult
+  putStrLn "noteOn"
+  pushEvent engine 
+    (EventConfig { eventConfig_time = 0, eventConfig_spaceId = 0, eventConfig_flags = []})
+    (NoteOn (NoteEvent { noteEvent_noteId = 0, noteEvent_portIndex = 0, noteEvent_channel = 0, noteEvent_key = 80, noteEvent_velocity = 80 }))
+  threadDelay $ 1000 * 1000
+  putStrLn "noteOff"
+  pushEvent engine 
+    (EventConfig { eventConfig_time = 1, eventConfig_spaceId = 0, eventConfig_flags = []})
+    (NoteOff (NoteEvent { noteEvent_noteId = 0, noteEvent_portIndex = 0, noteEvent_channel = 0, noteEvent_key = 80, noteEvent_velocity = 80 }))
+  threadDelay $ 1000 * 1000
+  putStrLn "stop"
+  stopResult <- stop engine
+  print stopResult
