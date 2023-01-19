a0a116e74c8cf097d7b30f9eefd565382fa1f4f7
Joe Betz <joebetz91@gmail.com>
2023-01-19 11:52:12 +0100

    track plugin and plugin process state

--- src/Clap/Host.hs
+++ src/Clap/Host.hs
@@ -3,7 +3,8 @@ module Clap.Host where
 import Clap.Extension
 import Clap.Interface.AudioBuffer
 import Clap.Interface.Entry as Entry
-import Clap.Interface.Plugin as Plugin
+import Clap.Interface.Plugin (PluginHandle, PluginDescriptor (..))
+import qualified Clap.Interface.Plugin as Plugin
 import Clap.Interface.Events
 import Clap.Interface.PluginFactory
 import Clap.Interface.Process
@@ -34,6 +35,7 @@ data PluginHost = PluginHost
     , pluginHost_process :: ProcessHandle
     , pluginHost_audioIn :: AudioBufferHandle
     , pluginHost_audioOut :: AudioBufferHandle
+    , pluginHost_events :: IORef [EventHandle]
     , pluginHost_inputEvents :: InputEventsHandle
     , pluginHost_outputEvents :: OutputEventsHandle
     , pluginHost_extensions :: HostExtensions
@@ -41,12 +43,23 @@ data PluginHost = PluginHost
 
 type PluginId = (FilePath, Int)
 
+data PluginState
+    = Inactive
+    | InactiveWithError
+    | ActiveAndSleeping
+    | ActiveAndProcessing
+    | ActiveWithError
+    | ActiveAndReadyToDeactivate
+    deriving (Eq, Show)
+
 data Plugin = Plugin
     { plugin_library :: PluginLibrary
     , plugin_entry :: PluginEntryHandle
     , plugin_factory :: PluginFactoryHandle
     , plugin_descriptor :: PluginDescriptor
     , plugin_handle :: PluginHandle
+    , plugin_state :: IORef PluginState
+    , plugin_processStatus :: IORef (Maybe ProcessStatus)
     }
 
 data PluginException
@@ -74,7 +87,8 @@ createPluginHost hostConfig = do
     process' <- createProcess
     audioIn <- createAudioBuffer
     audioOut <- createAudioBuffer
-    inputEvents <- createInputEvents
+    events <- newIORef []
+    inputEvents <- createInputEvents events
     outputEvents <- createOutputEvents
     pure $ PluginHost
         { pluginHost_handle = hostHandle 
@@ -83,6 +97,7 @@ createPluginHost hostConfig = do
         , pluginHost_process = process'
         , pluginHost_audioIn = audioIn
         , pluginHost_audioOut = audioOut
+        , pluginHost_events = events
         , pluginHost_inputEvents = inputEvents
         , pluginHost_outputEvents = outputEvents
         , pluginHost_extensions = extensions
@@ -110,40 +125,46 @@ load host (filePath, index) = do
                     Just pluginHandle -> do 
                         isPluginInitialized <- Plugin.init pluginHandle
                         unless isPluginInitialized $ throw PluginInitializationFailed
+                        state <- newIORef Inactive
+                        processStatus <- newIORef Nothing
                         addPlugin (filePath, index) $ Plugin
                             { plugin_library = library
                             , plugin_entry = entry
                             , plugin_factory = factory
                             , plugin_descriptor = descriptor
                             , plugin_handle = pluginHandle
+                            , plugin_state = state
+                            , plugin_processStatus = processStatus
                             }
     where        
         addPlugin :: PluginId -> Plugin -> IO ()
         addPlugin key plugin =
             modifyIORef (pluginHost_plugins host) $ Map.insert key plugin
 
-activate :: PluginHost -> PluginId -> Double -> Word32 -> IO Bool
+activate :: PluginHost -> PluginId -> Double -> Word32 -> IO ()
 activate host pluginId sampleRate blockSize = do
     plugin <- getPlugin host pluginId
-    Plugin.activate (plugin_handle plugin) sampleRate blockSize blockSize
+    isActivated <- Plugin.activate (plugin_handle plugin) sampleRate blockSize blockSize
+    setState plugin $ if isActivated 
+        then ActiveAndSleeping
+        else InactiveWithError 
 
 activateAll :: PluginHost -> Double -> Word32 -> IO ()
 activateAll host sampleRate blockSize = do
     plugins <- readIORef (pluginHost_plugins host) 
-    for_ (Map.elems plugins) $ \plugin -> do
-        isActivated <- Plugin.activate (plugin_handle plugin) sampleRate blockSize blockSize
-        unless isActivated $ throw ActivationFailed
+    for_ (Map.keys plugins) $ \pluginId ->
+        activate host pluginId sampleRate blockSize
 
 deactivate :: PluginHost -> PluginId -> IO ()
 deactivate host pluginId = do
     plugin <- getPlugin host pluginId
     Plugin.deactivate (plugin_handle plugin)
+    setState plugin Inactive
 
 deactivateAll :: PluginHost -> IO ()
 deactivateAll host = do
     plugins <- readIORef (pluginHost_plugins host) 
-    for_ (Map.elems plugins) $ \plugin ->
-        Plugin.deactivate (plugin_handle plugin)
+    for_ (Map.keys plugins) $ deactivate host
 
 processBegin :: PluginHost -> Word64 -> Int64 -> IO ()
 processBegin host framesCount steadyTime = do
@@ -154,7 +175,7 @@ processBegin host framesCount steadyTime = do
     
 processEvent :: PluginHost -> EventConfig -> Event -> IO ()
 processEvent host eventConfig event =
-    push (pluginHost_inputEvents host) eventConfig event
+    push (pluginHost_events host) eventConfig event
 
 process :: PluginHost -> IO ()
 process host = do
@@ -167,13 +188,15 @@ process host = do
     setAudioOutputs process' (pluginHost_audioOut host)
     setAudioOutputsCount process' 1
     plugins <- readIORef $ pluginHost_plugins host
-    for_ (Map.elems plugins) $ \plugin -> do
-        isStarted <- startProcessing (plugin_handle plugin)
-        if  isStarted 
-            then do  
-                processStatus <- Plugin.process (plugin_handle plugin) process'
-                print processStatus
-            else throw StartProcessingFailed
+    for_ (Map.elems plugins) $ \plugin -> do 
+        whenM (isPluginSleeping plugin) $ do
+            isStarted <- Plugin.startProcessing (plugin_handle plugin)
+            setState plugin $ if isStarted 
+                then ActiveAndProcessing
+                else ActiveWithError
+        whenM (isPluginProcessing plugin) $ do
+            status <- Plugin.process (plugin_handle plugin) process'
+            setProcessStatus plugin status
 
 processEnd :: PluginHost -> Word64 -> Int64 -> IO ()
 processEnd host numberOfFrames steadyTime = do
@@ -206,3 +229,15 @@ getPlugin host pluginId = do
     case Map.lookup pluginId plugins of
         Nothing -> throw InvalidPluginId
         Just plugin -> pure plugin
+
+setState :: Plugin -> PluginState -> IO ()
+setState plugin = writeIORef (plugin_state plugin)
+
+setProcessStatus :: Plugin -> ProcessStatus -> IO ()
+setProcessStatus plugin = writeIORef (plugin_processStatus plugin) . Just
+
+isPluginProcessing :: Plugin -> IO Bool
+isPluginProcessing plugin = (== ActiveAndProcessing) <$> readIORef (plugin_state plugin)
+
+isPluginSleeping :: Plugin -> IO Bool
+isPluginSleeping plugin = (== ActiveAndSleeping) <$> readIORef (plugin_state plugin)
