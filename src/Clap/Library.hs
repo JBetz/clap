43a1a62afff2b36c46e63ef227e041a205889352
Joe Betz <joebetz91@gmail.com>
2023-01-25 05:40:29 +0100

    recursive search for files with clap extension

--- src/Clap/Library.hs
+++ src/Clap/Library.hs
@@ -16,9 +16,18 @@ import Clap.Library.Windows
 #else
 import Clap.Library.POSIX
 #endif
+import Clap.Interface.Entry as Entry
+import Clap.Interface.Plugin
+import Clap.Interface.PluginFactory
 import Control.Monad
+import Data.Foldable
+import Data.List
+import Data.Maybe
+import Data.Traversable (for)
 import System.Directory
 import System.FilePath
+import System.IO.Unsafe (unsafeInterleaveIO)
+
 
 withPluginLibrary :: FilePath -> (PluginLibrary -> IO ()) -> IO ()
 withPluginLibrary filePath f = do
@@ -26,19 +35,40 @@ withPluginLibrary filePath f = do
     f pluginLibrary
     closePluginLibrary pluginLibrary
 
-scanForPlugins :: IO [FilePath]
+scanForPlugins :: IO [PluginDescriptor]
 scanForPlugins = do
     paths <- pluginLibraryPaths
     scanForPluginsIn paths
 
-scanForPluginsIn :: [FilePath] -> IO [FilePath]
+scanForPluginsIn :: [FilePath] -> IO [PluginDescriptor]
 scanForPluginsIn directories = do 
     paths <- traverse (\directory -> do 
         exists <- doesPathExist directory
         if exists 
             then do
-                paths <- listDirectory directory
+                paths <- findFilesWithExtension "clap" directory
                 pure $ (\path -> directory </> path) <$> paths
             else pure []
         ) directories
-    pure $ join paths
+    descriptors <- for (join paths) $ \filePath -> do
+        library <- openPluginLibrary filePath
+        entry <- lookupPluginEntry library
+        maybeFactory <- getFactory entry pluginFactoryId
+        case maybeFactory of
+            Just factory -> getPluginDescriptor factory 0
+            Nothing -> pure Nothing
+    pure $ catMaybes descriptors
+
+findFilesWithExtension :: String -> FilePath -> IO [FilePath]
+findFilesWithExtension extension filePath = do
+    allEntries <- listDirectory filePath
+    let relativeEntries = mkRel <$> allEntries
+    let matches = filter (\name -> ('.':extension) `isSuffixOf` name) relativeEntries
+    dirs <- filterM doesDirectoryExist relativeEntries
+    case dirs of
+        [] -> pure matches
+        ds -> do
+            next <- unsafeInterleaveIO $ foldMapA (findFilesWithExtension extension) ds
+            pure $ matches ++ next
+    where mkRel = (filePath </>)
+          foldMapA = (fmap fold .) . traverse
