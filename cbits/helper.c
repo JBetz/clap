638de77b883eff664f200b116f6fa45a8c5abbca
Joe Betz <joebetz91@gmail.com>
2023-01-27 07:38:04 +0100

    remove bindings-DSL macros from c helper

--- cbits/helper.c
+++ cbits/helper.c
@@ -1,13 +1,12 @@
-#include <bindings.cmacros.h>
 #include <clap/ext/audio-ports.h>
 #include <clap/ext/gui.h>
 #include <clap/plugin-invalidation.h>
 
-BC_GLOBALARRAY(CLAP_WINDOW_API_WIN32, char)
-BC_GLOBALARRAY(CLAP_WINDOW_API_COCOA, char)
-BC_GLOBALARRAY(CLAP_WINDOW_API_X11, char)
-BC_GLOBALARRAY(CLAP_WINDOW_API_WAYLAND, char)
-BC_GLOBALARRAY(CLAP_PLUGIN_INVALIDATION_FACTORY_ID, char)
-BC_GLOBALARRAY(CLAP_EXT_AUDIO_PORTS , char)
-BC_GLOBALARRAY(CLAP_PORT_MONO , char)
-BC_GLOBALARRAY(CLAP_PORT_STEREO , char)
+char *array_CLAP_WINDOW_API_WIN_32 (void) {return CLAP_WINDOW_API_WIN32;}
+char *array_CLAP_WINDOW_API_COCOA (void) {return CLAP_WINDOW_API_COCOA;}
+char *array_CLAP_WINDOW_API_X11 (void) {return CLAP_WINDOW_API_X11;}
+char *array_CLAP_WINDOW_API_WAYLAND (void) {return CLAP_WINDOW_API_WAYLAND;}
+char *array_CLAP_PLUGIN_INVALIDATION_FACTORY_ID (void) {return CLAP_PLUGIN_INVALIDATION_FACTORY_ID;}
+char *array_CLAP_EXT_AUDIO_PORTS (void) {return CLAP_EXT_AUDIO_PORTS;}
+char *array_CLAP_PORT_MONO (void) {return CLAP_PORT_MONO;}
+char *array_CLAP_PORT_STEREO (void) {return CLAP_PORT_STEREO;}
