71ccef4c5ae03b7c0e9fb4164de781aa930ea569
JBetz <joebetz91@gmail.com>
2024-04-28 17:03:43 +0200

    Cleanup

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  pkgs = import <nixpkgs> {};  # import ./pkgs.nix;
+  pkgs = import ./pkgs.nix;
   clap = pkgs.haskell.lib.overrideCabal (pkgs.haskellPackages.callCabal2nix "clap" ./. { }) {
     src = if pkgs.lib.inNixShell then null else ./.;
   };
@@ -9,14 +9,10 @@ in
       clap.env
     ];
     buildInputs = [
-      pkgs.cmake
-      pkgs.haskellPackages.c2hsc
-      pkgs.ghcid
       pkgs.cabal-install
-      pkgs.stylish-haskell
+      pkgs.ghcid
       pkgs.haskellPackages.ghc
       pkgs.haskellPackages.hoogle
       pkgs.haskellPackages.hlint
-      pkgs.haskellPackages.c2hs
     ];
   }
